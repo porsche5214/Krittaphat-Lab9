5990d6c97072380e252fff9a72ccf78f785ba231
Krittaphat <krittaphat_su@cmu.ac.th>
2024-12-13 15:40:28 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+int printO(int M , int N){
+	if (M <= 0 || N <=0){
+		cout << "Invalid input" ;
+	}else{
+		for (int i = 0 ; i < M ; i++){
+			for(int j = 0 ; j < N ; j++){
+				cout << "O" ;
+			}
+			cout << endl ;
+		}
+	}
+	return 0 ;
+}
 
 int main(){
 	
